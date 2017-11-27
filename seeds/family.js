4ec9f4061db6ef2c185af9521a4195696f293aae
Eddy Caldas <eddycaldas@live.com>
2017-11-27 16:14:08 -0700

    mode seeds

--- seeds/family.js
+++ seeds/family.js
@@ -15,6 +15,48 @@ exports.seed = function(knex, Promise) {
           kid2_name: 'Eddie Jr Caldas',
           kid2_birthday: '07-14-2001',
           kid2_POB: 'Denver, USA'
+        },
+        {
+          husband_name: 'Ronaldinho Velez',
+          husband_birthday: '07-06-1977',
+          husband_POB: 'Brasilia, Brasil',
+          wife_name: 'Rosie Martinez',
+          wife_birthday: '08-10-1981',
+          wife_POB: 'Las Vegas, USA',
+          kid1_name: 'Kayla Velez',
+          kid1_birthday: '10-22-2002',
+          kid1_POB: 'Miami, USA'
+        },
+        {
+          husband_name: 'Sam Supreme',
+          husband_birthday: '07-06-1997',
+          husband_POB: 'Colorado, USA',
+          wife_name: 'Nina Sky',
+          wife_birthday: '08-10-1999',
+          wife_POB: 'Las Vegas, USA',
+          kid1_name: 'Kayla Velez',
+          kid1_birthday: '02-26-2016',
+          kid1_POB: 'Texas, USA'
+        },
+        {
+          husband_name: 'Gabriel Iglesias',
+          husband_birthday: '07-06-1988',
+          husband_POB: 'California, USA',
+          wife_name: 'Mrs Shorty',
+          wife_birthday: '08-10-1989',
+          wife_POB: 'Washington DC, USA',
+          kid1_name: 'Little Pitbull',
+          kid1_birthday: '02-26-2011',
+          kid1_POB: 'Colorado, USA',
+          kid2_name: 'Daddy Yankee',
+          kid2_birthday: '08-16-2012',
+          kid2_POB: 'Colorado, USA',
+          kid3_name: 'Dylan Perez',
+          kid3_birthday: '01-23-2014',
+          kid3_POB: 'Colorado, USA',
+          kid4_name: 'Don Omar',
+          kid4_birthday: '02-26-2015',
+          kid4_POB: 'Colorado, USA'
         }
       ]);
     });
