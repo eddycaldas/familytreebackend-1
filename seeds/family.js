68c9754a15ebfce0c14b958ec15733ec75e1dd8d
Eddy Caldas <eddycaldas@live.com>
2017-11-28 11:50:42 -0700

    add pic bug2

--- seeds/family.js
+++ seeds/family.js
@@ -14,9 +14,11 @@ exports.seed = function(knex, Promise) {
           kid1_name: 'Katherine Caldas',
           kid1_birthday: '11-02-2011',
           kid1_POB: 'Colorado Springs, USA',
+          kid1_image: '../image/girl1.jpeg',
           kid2_name: 'Eddie Jr Caldas',
           kid2_birthday: '07-14-2001',
-          kid2_POB: 'Denver, USA'
+          kid2_POB: 'Denver, USA',
+          kid2_image: '../image/kid1.jpeg'
         },
         {
           husband_name: 'Ronaldinho Velez',
@@ -29,7 +31,8 @@ exports.seed = function(knex, Promise) {
           wife_image: '../image/mom2.jpeg',
           kid1_name: 'Kayla Velez',
           kid1_birthday: '10-22-2002',
-          kid1_POB: 'Miami, USA'
+          kid1_POB: 'Miami, USA',
+          kid1_image: '../image/girl2.jpeg'
         },
         {
           husband_name: 'Sam Supreme',
@@ -40,9 +43,10 @@ exports.seed = function(knex, Promise) {
           wife_birthday: '08-10-1999',
           wife_POB: 'Las Vegas, USA',
           wife_image: '../image/mom3.jpeg',
-          kid1_name: 'Kayla Velez',
+          kid1_name: 'Kayla Supreme',
           kid1_birthday: '02-26-2016',
-          kid1_POB: 'Texas, USA'
+          kid1_POB: 'Texas, USA',
+          kid1_image: '../image/girl3.jpeg'
         },
         {
           husband_name: 'Gabriel Iglesias',
@@ -56,16 +60,19 @@ exports.seed = function(knex, Promise) {
           kid1_name: 'Little Pitbull',
           kid1_birthday: '02-26-2011',
           kid1_POB: 'Colorado, USA',
+          kid1_image: '../image/kid2.jpeg',
           kid2_name: 'Daddy Yankee',
           kid2_birthday: '08-16-2012',
           kid2_POB: 'Colorado, USA',
+          kid2_image: '../image/kid3.jpeg',
           kid3_name: 'Dylan Perez',
-          wife_image: '../image/mom.jpeg',
           kid3_birthday: '01-23-2014',
           kid3_POB: 'Colorado, USA',
+          kid3_image: '../image/kid4.jpeg',
           kid4_name: 'Don Omar',
           kid4_birthday: '02-26-2015',
-          kid4_POB: 'Colorado, USA'
+          kid4_POB: 'Colorado, USA',
+          kid4_image: '../image/kid5.jpeg'
         }
       ]);
     });
